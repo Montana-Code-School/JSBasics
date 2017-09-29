d8ede89f8d6f193e3cad5c5504b174ef9a99ba27
Georgie Kirschner <georg.kirschner@icloud.com>
2017-09-29 17:54:05 -0600

    solution for removeDuplicates

---
+++ arrays/removeDuplicates.js
@@ -0,0 +1,15 @@
+// Write a JavaScript function removeDuplicates that removes duplicate values fom an array.
+
+var removeDuplicates= function(arr) {
+ var unique = []
+ arr.forEach((num, i) => {
+   if (unique.indexOf(num) > 0 ) {
+     arr.splice(i, 1);
+   }
+   else {
+     unique.push(num);
+   }
+ })
+
+// DO NOT DELETE
+module.exports = removeDuplicates;
