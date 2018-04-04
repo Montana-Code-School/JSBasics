1a4f187909642dd33de28a8639207e9d5d17f873
Georg Kirschner <gk@bridgerbowl.com>
2018-04-04 16:01:46 -0600

    recursion exercises

---
+++ recursion/factorial.js
@@ -0,0 +1,14 @@
+/**
+ * Implement a function that that computes a factorial recursively.
+ * Take the input number and multiply each preceding integer until you hit one.
+ * https://en.wikipedia.org/wiki/Factorial
+ * 
+ * @param {int} number - starting number 
+ */
+
+let factorial = (number) => {
+
+};
+
+// DO NOT DELETE
+module.exports = factorial;
