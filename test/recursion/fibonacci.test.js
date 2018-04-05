4c13e31ddd6d6c6a24ea2c1fd7df0e98e5324365
Georg Kirschner <gk@bridgerbowl.com>
2018-04-05 15:07:31 -0600

    fibonacci test

--- test/recursion/fibonacci.test.js
+++ test/recursion/fibonacci.test.js
@@ -1,13 +1,30 @@
-/**
- * Implement a function that computes the Fibonacci numbers up to the nth iteration
- * https://en.wikipedia.org/wiki/Fibonacci_number
- * 
- * @param {int} n - nr of iterations 
- */
+const chai = require('chai');
+var fibonacci = require('../../recursion/fibonacci.js');
 
-let fibonacci = (n) => {
-	
-};
+var expect = chai.expect;
 
-// DO NOT DELETE
-module.exports = fibonacci;
+xdescribe('Recursion Exercise - fibonacci()', () => {
+  it('should return fibonaccis number at nth position', () => {
+    expect(fibonacci(0)).toEqual(0);
+    expect(fibonacci(1)).toEqual(1);
+    expect(fibonacci(2)).toEqual(1);
+    expect(fibonacci(3)).toEqual(2);
+    expect(fibonacci(4)).toEqual(3);
+    expect(fibonacci(5)).toEqual(5);
+    expect(fibonacci(6)).toEqual(8);
+    expect(fibonacci(7)).toEqual(13);
+    expect(fibonacci(8)).toEqual(21);
+    expect(fibonacci(9)).toEqual(34);
+    expect(fibonacci(10)).toEqual(55);
+    expect(fibonacci(11)).toEqual(89);
+    expect(fibonacci(12)).toEqual(144);
+    expect(fibonacci(13)).toEqual(233);
+    expect(fibonacci(14)).toEqual(377);
+    expect(fibonacci(15)).toEqual(610);
+    expect(fibonacci(16)).toEqual(987);
+    expect(fibonacci(17)).toEqual(1597);
+    expect(fibonacci(18)).toEqual(2584);
+    expect(fibonacci(19)).toEqual(4181);
+    expect(fibonacci(20)).toEqual(6765);
+	});
+});
