8fa9d026f9ba72b0d18339ec0a63a62f672aed37
Georg Kirschner <gk@bridgerbowl.com>
2018-04-05 15:11:45 -0600

    update recursion tests

--- test/recursion/factorial.test.js
+++ test/recursion/factorial.test.js
@@ -3,17 +3,17 @@ var factorial = require('../../recursion/factorial.js');
 
 var expect = chai.expect;
 
-xdescribe('Recursion Exercise - factorial()', () => {
+describe('Recursion Exercise - factorial()', () => {
   it('should return factorials', () => {
-    expect(factorial(1)).toEqual(1);
-    expect(factorial(2)).toEqual(2);
-    expect(factorial(3)).toEqual(6);
-    expect(factorial(4)).toEqual(24);
-    expect(factorial(5)).toEqual(120);
-    expect(factorial(6)).toEqual(720);
-    expect(factorial(7)).toEqual(5040);
-    expect(factorial(8)).toEqual(40320);
-    expect(factorial(9)).toEqual(362880);
-    expect(factorial(10)).toEqual(3628800);
+    expect(factorial(1)).equal(1);
+    expect(factorial(2)).equal(2);
+    expect(factorial(3)).equal(6);
+    expect(factorial(4)).equal(24);
+    expect(factorial(5)).equal(120);
+    expect(factorial(6)).equal(720);
+    expect(factorial(7)).equal(5040);
+    expect(factorial(8)).equal(40320);
+    expect(factorial(9)).equal(362880);
+    expect(factorial(10)).equal(3628800);
 	});
 });
