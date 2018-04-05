d106bf9b560f8b9987a9a448bcc44bd496eb60a9
Georg Kirschner <gk@bridgerbowl.com>
2018-04-05 15:20:06 -0600

    solution for factorials

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -3,11 +3,16 @@
  * Take the input number and multiply each preceding integer until you hit one.
  * https://en.wikipedia.org/wiki/Factorial
  * 
- * @param {int} number - starting number 
+ * @param {int} num - starting number 
  */
 
-let factorial = (number) => {
-
+let factorial = (num) => {
+	if (num < 2) {
+		return 1;
+	}
+	else {
+		return num * factorial(num-1);
+	}
 };
 
 // DO NOT DELETE
