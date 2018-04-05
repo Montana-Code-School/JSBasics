b43a8d29cb3c15597478c4ee713642971893b863
Georg Kirschner <gk@bridgerbowl.com>
2018-04-05 15:14:01 -0600

    updating recursion tests again

--- test/recursion/factorial.test.js
+++ test/recursion/factorial.test.js
@@ -3,17 +3,17 @@ var factorial = require('../../recursion/factorial.js');
 
 var expect = chai.expect;
 
-describe('Recursion Exercise - factorial()', () => {
+xdescribe('Recursion Exercise - factorial()', () => {
   it('should return factorials', () => {
-    expect(factorial(1)).equal(1);
-    expect(factorial(2)).equal(2);
-    expect(factorial(3)).equal(6);
-    expect(factorial(4)).equal(24);
-    expect(factorial(5)).equal(120);
-    expect(factorial(6)).equal(720);
-    expect(factorial(7)).equal(5040);
-    expect(factorial(8)).equal(40320);
-    expect(factorial(9)).equal(362880);
-    expect(factorial(10)).equal(3628800);
+    expect(factorial(1)).to.eql(1);
+    expect(factorial(2)).to.eql(2);
+    expect(factorial(3)).to.eql(6);
+    expect(factorial(4)).to.eql(24);
+    expect(factorial(5)).to.eql(120);
+    expect(factorial(6)).to.eql(720);
+    expect(factorial(7)).to.eql(5040);
+    expect(factorial(8)).to.eql(40320);
+    expect(factorial(9)).to.eql(362880);
+    expect(factorial(10)).to.eql(3628800);
 	});
 });
--- test/recursion/fibonacci.test.js
+++ test/recursion/fibonacci.test.js
@@ -3,28 +3,28 @@ var fibonacci = require('../../recursion/fibonacci.js');
 
 var expect = chai.expect;
 
-xdescribe('Recursion Exercise - fibonacci()', () => {
+describe('Recursion Exercise - fibonacci()', () => {
   it('should return fibonaccis number at nth position', () => {
-    expect(fibonacci(0)).equal(0);
-    expect(fibonacci(1)).equal(1);
-    expect(fibonacci(2)).equal(1);
-    expect(fibonacci(3)).equal(2);
-    expect(fibonacci(4)).equal(3);
-    expect(fibonacci(5)).equal(5);
-    expect(fibonacci(6)).equal(8);
-    expect(fibonacci(7)).equal(13);
-    expect(fibonacci(8)).equal(21);
-    expect(fibonacci(9)).equal(34);
-    expect(fibonacci(10)).equal(55);
-    expect(fibonacci(11)).equal(89);
-    expect(fibonacci(12)).equal(144);
-    expect(fibonacci(13)).equal(233);
-    expect(fibonacci(14)).equal(377);
-    expect(fibonacci(15)).equal(610);
-    expect(fibonacci(16)).equal(987);
-    expect(fibonacci(17)).equal(1597);
-    expect(fibonacci(18)).equal(2584);
-    expect(fibonacci(19)).equal(4181);
-    expect(fibonacci(20)).equal(6765);
+    expect(fibonacci(0)).to.eql(1);
+    expect(fibonacci(1)).to.eql(1);
+    expect(fibonacci(2)).to.eql(1);
+    expect(fibonacci(3)).to.eql(2);
+    expect(fibonacci(4)).to.eql(3);
+    expect(fibonacci(5)).to.eql(5);
+    expect(fibonacci(6)).to.eql(8);
+    expect(fibonacci(7)).to.eql(13);
+    expect(fibonacci(8)).to.eql(21);
+    expect(fibonacci(9)).to.eql(34);
+    expect(fibonacci(10)).to.eql(55);
+    expect(fibonacci(11)).to.eql(89);
+    expect(fibonacci(12)).to.eql(144);
+    expect(fibonacci(13)).to.eql(233);
+    expect(fibonacci(14)).to.eql(377);
+    expect(fibonacci(15)).to.eql(610);
+    expect(fibonacci(16)).to.eql(987);
+    expect(fibonacci(17)).to.eql(1597);
+    expect(fibonacci(18)).to.eql(2584);
+    expect(fibonacci(19)).to.eql(4181);
+    expect(fibonacci(20)).to.eql(6765);
 	});
 });
