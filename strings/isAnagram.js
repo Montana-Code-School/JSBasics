82a34c681c60e997538e6619195ee6a0e17a9ca0
Georg Kirschner <gk@bridgerbowl.com>
2018-04-11 15:58:20 -0600

    anagram solution 1

--- strings/isAnagram.js
+++ strings/isAnagram.js
@@ -8,8 +8,22 @@
  * 
  */ 
 
+/*
+ * Approach 1: O(N log N); where N is the length of the string
+ */
 let isAnagram = (str1, str2) => {
 
+	// Strings of different lengths can not be permutations of each other
+	if (str1.length !== str2.length) {
+		return false;
+	}
+
+	// Sort both strings
+	var sortStr1 = str1.split('').sort().join('');
+	var sortStr2 = str2.split('').sort().join('');
+
+	// compare both strings
+	return (sortStr1 === sortStr2);
 };
 
 // DO NOT DELETE
