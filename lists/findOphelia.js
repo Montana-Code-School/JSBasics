8b2fc023a833db04bc7e1353e5de97fc36c6e0bf
Georgie Kirschner <georgie@montanacodeschool.com>
2018-09-25 14:04:18 -0600

    lists find ophi exercise

---
+++ lists/findOphelia.js
@@ -0,0 +1,15 @@
+/* Famous Quote: "Find is for idiots, because only idiots get lost. 
+ * I've never been lost, sometimes I take a lot of detours though."
+ * - Georgie Kirschner
+ */
+
+/* Review the animals.js file in this folder and the array of animal objects in it.
+ * Write a function findCat that returns the Ophelia the Cat.
+ * Try to come up with different variations of your function! 
+ * Try to solve it without using a for loop!
+ */
+
+var findOphelia;
+
+// DO NOT DELETE
+module.exports = findOphelia;
