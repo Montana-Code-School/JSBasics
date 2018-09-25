886f5424122b77b90d19657cd51e56fa0870b295
kramduckner <buckner.a.mark@gmail.com>
2018-09-25 07:41:41 -0600

    added exercises to /lists

---
+++ lists/map2.js
@@ -0,0 +1,20 @@
+var animals = require("./animals.js");
+
+// Maps are for idiots. I just use my instincts to travel everywhere. 
+// - Georgie Kirschner
+
+// In this exercise, instead of returning 'animal', return an object that includes everything in animal, 
+// but change the value of favoriteFood to be 'My favorite food is' + animal.favoriteFood.
+// i.e. animal Horse's object.favoriteFood = 'my favorite food is carrot'
+
+// P.S. Notice how we write a separate function and passed it into map. Cool, right?
+
+const addFavFoodStr = animal => {
+  return animal;
+};
+
+const mappedAnimals = animals.map(addFavFoodStr);
+
+
+// DO NOT DELETE
+module.exports = mappedAnimals;
---
+++ lists/map3.js
@@ -0,0 +1,37 @@
+var animals = require("./animals.js");
+
+// He's terrible at directions. He once drove us to the wrong state
+// because he was following his 'instincts.'
+// - Mark
+
+const likesCostumes = [
+  {
+    species: "Primate",
+    likesToBeCostumed: false
+  },
+  {
+    species: "Cat",
+    likesToBeCostumed: true
+  },
+  {
+    species: "Horse",
+    likesToBeCostumed: true
+  },
+  {
+    species: "Elephant",
+    likesToBeCostumed: true
+  },
+  {
+    species: "Bird",
+    likesToBeCostumed: false
+  }
+];
+
+// This will be trickier, but you can do it!
+// Using the map function, go over each object in the animals array and to that object append 
+// whether the animal likes to be costumed or not, using the likesCostumes array above. 
+
+// export your final array below.
+
+// DO NOT DELETE
+module.exports = ;
