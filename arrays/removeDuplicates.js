5116433d11f0a51e092880872dd05b4f7d8a2b6c
Georgie Kirschner <georgie@montanacodeschool.com>
2019-04-17 14:47:19 -0600

    removeDuplicates - bug fix for dubs in the beginning

--- arrays/removeDuplicates.js
+++ arrays/removeDuplicates.js
@@ -3,7 +3,7 @@
 var removeDuplicates = function(arr) {
   var unique = [];
   for (var i = 0; i < arr.length; i++) {
-    if (unique.indexOf(arr[i]) > 0) {
+    if (unique.indexOf(arr[i]) > -1) {
       arr.splice(i, 1);
     } else {
       unique.push(arr[i]);
